fe7bb3c072ce2e77fc5837d84b9c33e893c5c38b
Shaofeng Zhu <shaozhu@microsoft.com>
2020-07-26 17:36:30 -0700

    Update

--- webapp/functions.py
+++ webapp/functions.py
@@ -1,4 +1,7 @@
 from officepy import customfunctions
+import random
+import pandas as pd
+import numpy as np
 
 @customfunctions.customfunction(name = "MYADD")
 def myadd(x, y):
@@ -7,3 +10,33 @@ def myadd(x, y):
 @customfunctions.customfunction(name = "BADADD")
 def badadd(x, y):
 	return x + y + 1
+
+@customfunctions.customfunction(
+	name="SUMTABLE", 
+	parameters = [
+		customfunctions.ParameterInfo(dimensionality=customfunctions.Dimensionality.matrix)
+	]
+	)
+def sum_table(input):
+	sum = 0
+	for row in input:
+		for item in row:
+			sum = sum + item
+	return sum
+
+@customfunctions.customfunction(
+	name="GETTABLE",
+	resultDimensionality = customfunctions.Dimensionality.matrix
+	)
+def get_table(row, column):
+	return [[random.random() for c in range(column)] for r in range(row)]
+
+@customfunctions.customfunction(
+	name="GETPANDAS",
+	resultDimensionality = customfunctions.Dimensionality.matrix
+	)
+def get_pandas(rows, columns):
+    data = np.random.rand(rows, columns)
+    column_names = [chr(ord('A') + x) for x in range(columns)]
+    df = pd.DataFrame(data, columns=column_names)
+    return df
