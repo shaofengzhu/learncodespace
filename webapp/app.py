f206bce1104fb230bceb88ee455da379da29d8d0
Shaofeng Zhu <shaozhu@microsoft.com>
2020-07-28 11:25:57 -0700

    Use prod

--- webapp/app.py
+++ webapp/app.py
@@ -37,7 +37,7 @@ def agave_functions():
 
 @app.route("/functions.html", methods = ["GET"])
 def agave_page():
-	responseText = customfunctions.getPageHtml(True)
+	responseText = customfunctions.getPageHtml()
 	response = Response(responseText)
 	response.headers["Content-Type"] = "text/html"
 	return response
